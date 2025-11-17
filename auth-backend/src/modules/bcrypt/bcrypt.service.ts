a50fddc838aad6b80be4f38114cbacea8482011f
chrlr <yosoypro_117@hotmail.com>
2025-11-16 21:34:14 -0600

    bcrypt module:service

---
+++ auth-backend/src/modules/bcrypt/bcrypt.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class BcryptService {
+  private readonly saltRounds = 12;
+
+  async hash(data: string): Promise<string> {
+    const salt = await bcrypt.genSalt(this.saltRounds);
+
+    return await bcrypt.hash(data, salt);
+  }
+
+  async compare(data: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(data, hash);
+  }
+}
